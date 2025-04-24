021cc875abf36cacba9f5cb7f7d23d47b2ecdeb7
Mike Palacio <mpalacio123@gmail.com>
2025-04-24 01:33:31 -0500

    tag main branch when a pr is closed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout';
-import LoginPanel from './components/LoginPanel';
 import HomePage from './pages/HomePage';
-import AuthProvider from './components/AuthProvider';
 import './index.css';
 import LibraryPage from './pages/LibraryPage';
 
@@ -12,12 +10,6 @@ export default function App() {
         <Routes>
           <Route path="/home" element={<HomePage />} />
           <Route path="/library" element={<LibraryPage />} />
-          {/* // <Route path="/fetch-data" element={<FetchData />} /> */}
-          {/* <Route path="/search" element={<SearchWrapped />}>
-            <Route path=":imdbId" element={<SearchResultWrapped />} />
-          </Route> */}
-          {/* <Route path="/queue" element={<Queue />} /> */}
-          {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </Layout>
     );
