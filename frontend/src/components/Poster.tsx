70ef373ba2c3d7e661970289db24653ecc4a8495
Mike Palacio <mpalacio123@gmail.com>
2025-08-22 21:50:38 -0500

    Add smoother loading for library shelf

--- frontend/src/components/Poster.tsx
+++ frontend/src/components/Poster.tsx
@@ -1,4 +1,5 @@
 import { MoviePartial } from "@/types/library";
+import { useState } from "react";
 import "./Poster.css";
 
 interface PosterProps {
@@ -6,6 +7,8 @@ interface PosterProps {
 }
 
 export default function Poster({ movie }: PosterProps) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   const isRecentlyAdded = (() => {
     const now = new Date();
     const twoWeeksAgo = new Date();
@@ -13,13 +16,18 @@ export default function Poster({ movie }: PosterProps) {
     return movie.addedDate >= twoWeeksAgo;
   })();
 
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <div className="poster-container">
       {isRecentlyAdded && <div className="poster-banner">Recently Added</div>}
       <img
         src={movie.poster}
         alt={movie.title}
-        className="poster-image"
+        className={`poster-image ${imageLoaded ? 'loaded' : ''}`}
+        onLoad={handleImageLoad}
       />
     </div>
   );
